c71481e0771ae8d62ff7c7365e40d4520e0f83a3
樺島祐貴 <kabashimayuuki@kabashimayuukinoMacBook-Air.local>
2024-10-03 18:02:43 +0900

    “db接続”

--- reviewproject/settings.py
+++ reviewproject/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 import os
 from pathlib import Path
+import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -75,11 +76,20 @@ WSGI_APPLICATION = 'reviewproject.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
+env = environ.Env()
+
+READ_ENV_FILE = env.bool('DJANGO_READ_ENV_FILE' , default=True)
+if READ_ENV_FILE:
+    env_file = str(BASE_DIR)+'/.env'
+    env.read_env(env_file)
 
 DATABASES = {
    'default': {
-       'ENGINE': 'django.db.backends.sqlite3',
-       'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': env('POSTGRES_DATABASE'),
+        'USER': env('POSTGRES_USER'),
+        'PASSWORD': env('POSTGRES_PASSWORD'),
+        'HOST': env('POSTGRES_HOST'),
    }
 }
 
