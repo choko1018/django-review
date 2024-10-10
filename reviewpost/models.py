f3c71687b7ca4a33c1991ecf119b1d0992e8afce
choko1018 <choko1018@users.noreply.github.com>
2024-10-10 18:19:16 +0900

    blob接続

--- reviewpost/models.py
+++ reviewpost/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from vercel_storage import blob
 # Create your models here.
 class ReviewModel(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -8,3 +8,15 @@ class ReviewModel(models.Model):
     content = models.TextField()
     useful_num = models.IntegerField(null = True, blank = True, default = 0)
     product_image = models.ImageField(upload_to='',null = True, blank=True)
+    product_image_url = models.URLField(null=True,blank=True)
+    def save(self, *args, **kwargs):
+       if self.product_image:
+           resp = blob.put(
+               pathname=self.product_image.name,
+               body=self.product_image,
+               options={'token': settings.vercel_blob_token}
+           )
+           self.product_image = None
+           self.product_image_url = resp['url'] 
+       # 通常の保存処理を実行
+       super().save(*args, **kwargs)
